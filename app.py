20408b6d9f324c5b55a313a38d0e2801832e1a3e
deepakss74 <100316769+deepakss74@users.noreply.github.com>
2025-04-21 23:35:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,5 +35,5 @@ def get_Chat_response(text):
     return response
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 1000))
     app.run(host='0.0.0.0', port=port)
